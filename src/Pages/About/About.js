2f497182dd01a4f40cbb73336ed083df72465f17
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-04-18 19:46:29 +0600

    edit readmi file

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -8,7 +8,9 @@ const About = () => {
     <img className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 shadow-lg object-cover object-center rounded" alt="hero" src={aboutMe}/>
     <div className="text-center lg:w-2/3 w-full">
       <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">MD : SHIHAB UDDIN</h1>
-      <p className="mb-8 leading-relaxed">Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing tousled. Chambray dreamcatcher trust fund, kitsch vice godard disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh. Pour-over meditation PBR&B pickled ennui celiac mlkshk freegan photo booth af fingerstache pitchfork.</p>
+      <p className="mb-8 leading-relaxed">
+        I am MD SHIHAB UDDIN . I want to become a full stack developer . I will hard work to achieve my goal . I will collect information from various resource for achieve my goal . I will complete programming hero " complete web developing course " properly . Then I will try to get an Internship . If I will get an internship I will continue my learning properly.
+      </p>
   
     </div>
   </div>
